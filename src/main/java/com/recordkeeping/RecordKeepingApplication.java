3b949f22ebc09358804d0ca0b3f1efd56ceaf2f5
Andres Suarez <ansuarez9@gmail.com>
2018-08-22 00:01:21 -0400

    added CORS

--- src/main/java/com/recordkeeping/RecordKeepingApplication.java
+++ src/main/java/com/recordkeeping/RecordKeepingApplication.java
@@ -2,7 +2,11 @@ package com.recordkeeping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.recordkeeping.CORS.CORSFilter;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -11,4 +15,14 @@ public class RecordKeepingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecordKeepingApplication.class, args);
 	}
+	
+	@Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registrationBean =
+                new FilterRegistrationBean(new CORSFilter());
+        registrationBean.setName("CORS Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
 }
